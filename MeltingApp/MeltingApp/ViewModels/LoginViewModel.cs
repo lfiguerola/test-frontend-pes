d875495441dfc9974dd8aac44780f9f0d1fd6849
lfiguerola <laurafipe@gmail.com>
2018-04-10 11:27:18 +0200

    api client initial setup

---
+++ MeltingApp/MeltingApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using MeltingApp.Interfaces;
+using MeltingApp.Models;
+using MeltingApp.Services;
+using MeltingApp.Views.Pages;
+using Xamarin.Forms;
+
+namespace MeltingApp.ViewModels
+{
+    public class LoginViewModel : BindableObject
+    {
+        private INavigationService _navigationService;
+        private IApiClientService _apiClientService;
+        private User _user;
+
+        public User User
+        {
+            get { return _user; }
+            set
+            {
+                _user = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+
+        public LoginViewModel()
+        {
+            _navigationService = DependencyService.Get<INavigationService>();
+            _apiClientService = DependencyService.Get<IApiClientService>();
+            LoginCommand = new Command(HandleLoginCommand);
+        }
+        public Command LoginCommand { get; set; }
+
+        async void HandleLoginCommand()
+        {
+            User = new User()
+            {
+                email = "alex.cmillan@outlook.com", code = "1151a821"
+            };
+            await _apiClientService.PostAsync<User>(User, "Activate");
+            //navigationService.SetRootPage<MainPage>();
+        }
+    }
+}
